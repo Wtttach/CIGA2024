0833a58233f9851eced6a1bf7e92937336926c93
Viking-wei <74452603+Viking-wei@users.noreply.github.com>
2024-07-07 19:58:01 +0800

    调整

--- Assets/Scripts/Finish.cs
+++ Assets/Scripts/Finish.cs
@@ -11,11 +11,19 @@ public class Finish : MonoBehaviour
     public List<float> LevelRotationKey;
     public Vector3 Level2PlatformTargetPos;
     public Transform Level2Platform;
+    public float Level3key1;
+    public float Level3key2;
+    public float Level3key3;
+    public Transform Level3Platform;
+    public Vector3 Level3PlatformTargetPos1;
+    public Vector3 Level3PlatformTargetPos2;
+    private int _finalFlag = 0;
     private int _currentLevel;
     private float _timer=0;
     public AudioSource FinishAudio = null;
     private void Start()
     {
+        Debug.Log(1|2|4);
         _currentLevel = 0;
         Levels[_currentLevel].SetActive(true);
         Debug.Log(Levels[_currentLevel].transform.eulerAngles);
@@ -62,6 +70,52 @@ public class Finish : MonoBehaviour
             else
             {
                 _timer = 0;
+            }
+        }
+        else if (_currentLevel == 2)
+        {
+            Debug.Log(Level3Platform.position);
+            Debug.Log(Levels[2].transform.eulerAngles.y);
+            if (Vector3.Distance(Level3Platform.position, Level3PlatformTargetPos1) < 0.5f &&
+                (Mathf.Abs(Levels[2].transform.eulerAngles.y - Level3key1) < 2f ||
+                 Mathf.Abs(Levels[2].transform.eulerAngles.y - Level3key1) > 358f))
+            {
+                _timer += Time.deltaTime;
+                if (_timer >= 0.6)
+                {
+                    Debug.LogError("1!");
+                    _finalFlag|=1;
+                    _timer = 0;
+                }
+            }
+            if (Vector3.Distance(Level3Platform.position, Level3PlatformTargetPos2) < 0.5f &&
+                (Mathf.Abs(Levels[2].transform.eulerAngles.y - Level3key2) < 2f ||
+                 Mathf.Abs(Levels[2].transform.eulerAngles.y - Level3key2) > 358f))
+            {
+                _timer += Time.deltaTime;
+                if (_timer >= 0.6)
+                {
+                    Debug.LogError("2");
+                    _finalFlag|=2;
+                    _timer = 0;
+                }
+            }
+
+            if (Mathf.Abs(Levels[2].transform.eulerAngles.y - Level3key3) < 2f ||
+                Mathf.Abs(Levels[2].transform.eulerAngles.y - Level3key3) > 358f)
+            {
+                _timer += Time.deltaTime;
+                if (_timer >= 0.6)
+                {
+                    Debug.LogError("3!");
+                    _finalFlag|=4;
+                    _timer = 0;
+                }
+            }
+            if (_finalFlag==7)
+            {
+                Debug.LogError("finish!");
+                Levels[2].SetActive(false);
             }
         }
     }
